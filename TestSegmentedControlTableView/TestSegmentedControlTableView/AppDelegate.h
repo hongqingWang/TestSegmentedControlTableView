26c387a156c09a2356c2e7c77bae02bf50e12123
王红庆 <272338444@qq.com>
2019-07-26 17:12:27 +0800

    初始化项目
    初始化项目

---
+++ TestSegmentedControlTableView/TestSegmentedControlTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSegmentedControlTableView
+//
+//  Created by JiuFuKeJi on 2019/7/26.
+//  Copyright © 2019 JiuFuKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSegmentedControlTableView/TestSegmentedControlTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSegmentedControlTableView
+//
+//  Created by JiuFuKeJi on 2019/7/26.
+//  Copyright © 2019 JiuFuKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
